93982a31994b75b27a010aa1991a77bd3748c6a8
NikoTrif <nikotrif@gmail.com>
2023-03-13 12:22:30 +0100

    added SearchSelect component

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,105 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import SelectSearch from 'react-select-search';
-import 'react-select-search/style.css';
-
+import SearchSelect from './SearchSelect';
 
 function Head() {
-    const state = useSelector(state => state);
-    const [inpVal, setInpVal] = useState('');
-    const [inpOneVal, setOneVal] = useState('');
-    const { world: { world, loading, error } } = state;
-
-    class Option {
-        constructor(lat, lon, city, country) {
-            this.lat = lat;
-            this.lon = lon;
-            this.city = city;
-            this.country = country;
-        }
-    }
-
-    useEffect(() => {
-        const timeOutValue = setTimeout(() => {
-            setOneVal(inpVal)
-        }, 500);
-        return () => clearTimeout(timeOutValue);
-    }, [inpVal, inpOneVal]);
-
-    function LoadInput(inpOneVal, world, loading, error) {
-        let searcherVal = inpOneVal
-            .replaceAll(',', '$')
-            .replaceAll('$ ', '$')
-            .split('$');
-
-        if (world !== undefined) {
-            if (inpOneVal.length > 0) {
-                let city = [
-                    ...new Set([
-                        ...world.filter(ea => ea.city.toLowerCase().includes(searcherVal[0].toLowerCase())),
-                        ...world.filter(eb => eb.admin_name?.toLowerCase()?.includes(searcherVal[0].toLowerCase())),
-                        ...world.filter(ec => ec.city_ascii?.toLowerCase().includes(searcherVal[0].toLowerCase()))
-                    ])
-                ];
-
-                if (searcherVal.length > 1) {
-                    city = city.filter(e => e.country.toLowerCase().includes(searcherVal[1].toLowerCase()));
-                }
-
-                // console.log(city);
-
-                if (city.length > 0) {
-                    return city;
-                }
-                else {
-                    return [];
-                }
-            }
-            return [];
-        }
-        return [];
-    }
-
-    let filteredCities = [];
-    let options = [];
-    if (inpOneVal === inpVal) {
-        filteredCities = LoadInput(inpOneVal, world, loading, error);
-        // console.log(filteredCities);
-    }
-    else {
-        filteredCities = [];
-    }
-
-    if (filteredCities !== []) {
-        filteredCities.map((city, i) => {
-            // let opt = new Option(city.lat, city.lng, city.city, city.country);
-            // console.log(opt);
-            options.push({
-                name: city.city + ', ' + city.country,
-                value: {
-                    lat: city.lat,
-                    lon: city.lng
-                }
-            });
-        });
-        console.log(options);
-    }
-
     return (
         <div>
             <h1>Reactor Weather</h1>
             <h2><button>Beograd, Srbija</button></h2>
-            <input type="text" id='cityInput' onChange={e => { setInpVal(e.target.value) }} />
-            <div>
-                {/* <select name='CitySelect' id='citySelect' multiple aria-label='multiple select'>{
-                    filteredCities.map((city, i) => {
-                        if (filteredCities.length !== 0) {
-                            return <option value={`${city.lat},${city.lon}`} key={`opt${i}`}>{city.city}, {city.country}</option>
-                        }
-                    })
-                }</select> */}
-                <SelectSearch options={options} search={true} />
-            </div>
+            <SearchSelect />
         </div >
     );
 }
---
+++ src/components/SearchSelect.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState, Fragment } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchWeatherAction } from '../redux/slices/weatherSlices';
+
+function SearchSelect() {
+    const state = useSelector(state => state);
+    const [inpVal, setInpVal] = useState('');
+    const [inpOneVal, setOneVal] = useState('');
+    const { world: { world, loading, error } } = state;
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const timeOutValue = setTimeout(() => {
+            setOneVal(inpVal)
+        }, 500);
+        return () => clearTimeout(timeOutValue);
+    }, [inpVal, inpOneVal]);
+
+    function LoadInput(inpOneVal, world, loading, error) {
+        let searcherVal = inpOneVal
+            .replaceAll(',', '$')
+            .replaceAll('$ ', '$')
+            .split('$');
+
+        if (world !== undefined) {
+            if (inpOneVal.length > 0) {
+                let city = [
+                    ...new Set([
+                        ...world.filter(ea => ea.city.toLowerCase().includes(searcherVal[0].toLowerCase())),
+                        ...world.filter(eb => eb.admin_name?.toLowerCase()?.includes(searcherVal[0].toLowerCase())),
+                        ...world.filter(ec => ec.city_ascii?.toLowerCase().includes(searcherVal[0].toLowerCase()))
+                    ])
+                ];
+
+                if (searcherVal.length > 1) {
+                    city = city.filter(e => e.country.toLowerCase().includes(searcherVal[1].toLowerCase()));
+                }
+
+                // console.log(city);
+
+                if (city.length > 0) {
+                    return city;
+                }
+                else {
+                    return [];
+                }
+            }
+            return [];
+        }
+        return [];
+    }
+
+    let filteredCities = [];
+    let options = [];
+    if (inpOneVal === inpVal) {
+        filteredCities = LoadInput(inpOneVal, world, loading, error);
+        // console.log(filteredCities);
+    }
+    else {
+        filteredCities = [];
+    }
+
+    function selectOnClick(optionValue) {
+        const coords = optionValue.split(',');
+        const coord = {
+            lat: coords[0],
+            lon: coords[1]
+        }
+        dispatch(fetchWeatherAction(coord));
+        dispatch(fetchWeatherAction(coord))
+
+    }
+
+    return (
+        <Fragment>
+            <input type="text" id='cityInput' onChange={e => { setInpVal(e.target.value) }} />
+            <div>
+                <select name='CitySelect' id='citySelect' multiple aria-label='multiple select'>{
+                    filteredCities.map((city, i) => {
+                        if (filteredCities.length !== 0) {
+                            return <option value={`${city.lat},${city.lng}`} key={`opt${i}`} onClick={e => {
+                                selectOnClick(e.target.value)
+                            }}>{city.city}, {city.country}</option>
+                        }
+                    })
+                }</select>
+            </div>
+        </Fragment>
+    );
+}
+
+export default SearchSelect;
