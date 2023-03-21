38b3460f12d54164c4ef77e9c6c3a1f8ba320b0b
NikoTrif <nikotrif@gmail.com>
2023-03-21 20:24:52 +0100

    hourForecast key error repaired

--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -2,6 +2,7 @@ import React from 'react';
 import OneHourForecast from './OneHourForecast';
 
 function HourlyForecast(props) {
+    let i = 0;
     console.log('HoursForecast', props.hourlyForecast[0]);
 
     return (
@@ -9,9 +10,10 @@ function HourlyForecast(props) {
             <h3>HourlyForecast</h3>
             {
                 props.hourlyForecast.map(h => {
-                    return <OneHourForecast oneHourForecast={h} />
+                    return <OneHourForecast key={(i++)} oneHourForecast={h} />
                 })
             }
+
         </div>
     );
 }
