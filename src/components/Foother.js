aaf8a23efda6cd4e35155ea2763fc1e4a870c466
NikoTrif <nikotrif@gmail.com>
2023-04-01 20:49:07 +0200

    footer styled

--- src/components/Foother.js
+++ src/components/Foother.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import '../styles/sass/footer.scss';
+
 function Foother() {
     return (
-        <p>Foother</p>
+        <footer>
+            <small>Copyright © 2023 Nikola Trifunović</small>
+            <a href="https://linkedin.com/in/nikola-trifunovic-2a332b233">
+                <i className='linkedin icon' />
+            </a>
+        </footer>
     );
 }
 
