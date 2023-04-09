2227a531c677d8f8a5cb4a11fcf6b69fc9e3efb7
NikoTrif <nikotrif@gmail.com>
2023-04-09 12:24:34 +0200

    set background functionality template

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import clearD from '../video/clear-d.mp4';
 import clearN from '../video/clear-n.mov';
-import cloudD from '../video/cloud-d.mp4';
-import cloudN from '../video/cloud-n.mp4';
+import cloudsD from '../video/cloud-d.mp4';
+import cloudsN from '../video/cloud-n.mp4';
 import mistD from '../video/mist-d.mp4';
 import mistN from '../video/mist-n.mp4';
 import rainD from '../video/rain-d.mp4';
@@ -14,17 +15,66 @@ import snowD from '../video/snow-d.mp4';
 import snowN from '../video/snow-n.mp4';
 
 import '../styles/sass/background.scss';
-import { useSelector } from 'react-redux';
+import { includes } from 'lodash';
 
-function Background(props) {
+function Background() {
     const state = useSelector(state => state);
 
     const { weather: { loading, error, weather } } = state;
 
     function SelectBackground() {
+        let timeNow = new Date();
         let sunrise = new Date(weather?.sys?.sunrise * 1000);
         let sunset = new Date(weather?.sys?.sunset * 1000);
-        let misty = ["Mist",]
+        let misty = ["Mist", "Smoke", "Haze", "Fog"];
+        let conditionExtracted = weather?.weather[0]?.main;
+
+        switch (conditionExtracted) {
+            case 'Clear':
+                if (timeNow > sunrise && timeNow < sunset) {
+                    return clearD;
+                }
+                else {
+                    return clearN;
+                }
+            case 'Clouds':
+                if (timeNow > sunrise && timeNow < sunset) {
+                    return cloudsD;
+                }
+                else {
+                    return cloudsN;
+                }
+            case includes(misty):
+                if (timeNow > sunrise && timeNow < sunset) {
+                    return mistD;
+                }
+                else {
+                    return mistN;
+                }
+            case 'Rain':
+                if (timeNow > sunrise && timeNow < sunset) {
+                    return rainD;
+                }
+                else {
+                    return rainN;
+                }
+            case 'Thunderstorm':
+                if (timeNow > sunrise && timeNow < sunset) {
+                    return thunderD;
+                }
+                else {
+                    return thunderN;
+                }
+            case 'Snow':
+                if (timeNow > sunrise && timeNow < sunset) {
+                    return snowD;
+                }
+                else {
+                    return snowN;
+                }
+            default:
+                return clearD;
+        }
 
     }
 
@@ -39,10 +89,9 @@ function Background(props) {
             )
         }
         else {
-            SelectBackground();
             return (
                 <video className='video-background' autoPlay loop muted>
-                    <source src={snowN} type='video/mp4' />
+                    <source src={SelectBackground()} type='video/mp4' />
                 </video>
             )
         }
