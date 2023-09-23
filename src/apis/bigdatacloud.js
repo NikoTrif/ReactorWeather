4cefc4795554bfe36faf878a020d776b1d5c5b32
NikoTrif <nikotrif@gmail.com>
2023-09-23 17:48:33 +0200

    bigdatacloud api key changed

--- src/apis/bigdatacloud.js
+++ src/apis/bigdatacloud.js
@@ -1,4 +1,4 @@
 export function callCity(lat, lon) {
-    const apiKey = 'bdc_9d55ada394ce43cea64fbc2f74a906df';
+    const apiKey = 'bdc_d25b293ef9434e27a8412adc53ec5958';
     return `https://api-bdc.net/data/reverse-geocode?latitude=${lat}&longitude=${lon}&localityLanguage=en&key=${apiKey}`
 }
